1e54b14187cb21c03ae5aced221ecab0257e5a07
Phelim Bradley <wave@phel.im>
2016-10-19 10:57:12 +0100

    replace disttools with seqtuptools

--- pyseqfile/main.py
+++ pyseqfile/main.py
@@ -5,8 +5,12 @@
 #     pass
 
 from seqfile import Reader
-
-reader = Reader('/Users/phelimb/Documents/data/5054-08.fastq')
+# reader = Reader('/Users/phelimb/Documents/data/5054-08.fastq')
+reader = Reader('/Users/phelimb/Documents/git/atlas-seq/oxa1.fasta')
 for r in reader:
-    print(r)  # for r in reader:
+    r = r.decode("UTF-8")
+    print(r)
+    # print(k)
+
+    # print(r)  # for r in reader:
 #     print(r)
